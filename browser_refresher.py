15475cbe22f2ac2cf128955322cb0d90806d71f0
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-04-27 08:52:40 +0100

    Update browser_refresher.py

--- browser_refresher.py
+++ browser_refresher.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-browser = webdriver.Chrome() # Change Chrome to Firefox, Edge, Ie or Safari, depending on what browser you want
+browser = webdriver.Chrome() # Change Chrome to Firefox, Edge, Ie or Safari, depending on what browser you want (NOTE: it has to also be installed on your computer for the browser to open)
 
 browser.get(input("Enter in the url you want to navigate to: "))
 
